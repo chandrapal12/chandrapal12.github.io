31cba36beb9656b6b9a9cea472796cc72c18a756
Aman <amankeshari268@gmail.com>
2022-06-03 17:58:17 +0530

    Initial commit

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+console.log("Script running...")
+// Hide Cross Icon
+document.querySelector('.close').style.display='none';
+
+// Sidebar go and come
+document.querySelector(".hamburger").addEventListener("click", ()=>{
+    document.querySelector(".sidebar").classList.toggle('sidebarGo');
+    if(document.querySelector(".sidebar").classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display = 'inline'
+        document.querySelector('.close').style.display='none'
+    }
+    else{
+        document.querySelector('.ham').style.display = 'none'
+        setTimeout(() => {
+            document.querySelector('.close').style.display='inline'
+        },400);
+            
+    }
+
+})
+
